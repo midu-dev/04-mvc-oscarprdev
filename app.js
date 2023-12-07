ad1c6687a2111db7f9a7debdf08246c2fc8a6d01
oscarprdev <vlcoscar8@gmail.com>
2023-12-07 09:51:19 +0100

    fix: add Number() to delete task id

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-import express from 'express'
-import { tasksRoutes } from './routes/tasks.js'
+import express from 'express';
+import { tasksRoutes } from './routes/tasks.js';
 
-export const app = express()
+export const app = express();
 
-app.use(express.json())
-app.use(tasksRoutes)
+app.use(express.json());
+app.use(tasksRoutes);
 
-const PORT = 1234
+const PORT = 1234;
 
 export const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+	console.log(`Server is running on port ${PORT}`);
+});
--- models/task.js
+++ models/task.js
@@ -2,13 +2,13 @@
  * Array to store tasks.
  * @type {Array<Object>}
  */
-const tasks = []
+const tasks = [];
 
 /**
  * ID for the next task. Increments each time a new task is added.
  * @type {number}
  */
-let currentId = 1
+let currentId = 1;
 
 /**
  * Model class representing a task.
@@ -20,48 +20,48 @@ let currentId = 1
  *
  * */
 export class TaskModel {
-  /**
-   * Fetches all tasks.
-   * @name TaskModel.getAllTasks
-   * @static
-   * @returns {Array<Object>} Array of tasks.
-   */
-  static getAllTasks () {
-    return tasks
-  }
+	/**
+	 * Fetches all tasks.
+	 * @name TaskModel.getAllTasks
+	 * @static
+	 * @returns {Array<Object>} Array of tasks.
+	 */
+	static getAllTasks() {
+		return tasks;
+	}
 
-  /**
- * Adds a new task to the list.
- * @name TaskModel.addTask
- * @static
- * @param {string} description - The description of the task.
- * @returns {Object} The new task object.
- */
-  static addTask (description) {
-    const newTask = {
-      id: currentId++,
-      description
-    }
-    tasks.push(newTask)
+	/**
+	 * Adds a new task to the list.
+	 * @name TaskModel.addTask
+	 * @static
+	 * @param {string} description - The description of the task.
+	 * @returns {Object} The new task object.
+	 */
+	static addTask(description) {
+		const newTask = {
+			id: currentId++,
+			description,
+		};
+		tasks.push(newTask);
 
-    return newTask
-  }
+		return newTask;
+	}
 
-  /**
- * Deletes a task from the list by its ID.
- * @name TaskModel.deleteTask
- * @static
- * @param {number} id - The ID of the task to delete.
- */
-  static deleteTask (id) {
-    const taskIndex = tasks.findIndex((task) => task.id === id)
+	/**
+	 * Deletes a task from the list by its ID.
+	 * @name TaskModel.deleteTask
+	 * @static
+	 * @param {number} id - The ID of the task to delete.
+	 */
+	static deleteTask(id) {
+		const taskIndex = tasks.findIndex((task) => task.id === Number(id));
 
-    if (taskIndex >= 0) {
-      tasks.splice(taskIndex, 1)
+		if (taskIndex >= 0) {
+			tasks.splice(taskIndex, 1);
 
-      return tasks
-    }
+			return tasks;
+		}
 
-    throw new Error("Task to be deleted doesn't exist")
-  }
+		throw new Error("Task to be deleted doesn't exist");
+	}
 }
